f9413849440d21456e378d6bd56b1b4c18964e7d
KD4-sima-83599 <162394288+KD4-sima-83599@users.noreply.github.com>
2025-03-16 15:23:36 +0530

    dbo.User.sql uploded

---
+++ dbo.User.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[User] (
+    [UserId]      INT          IDENTITY (1, 1) NOT NULL,
+    [FirstName]   VARCHAR (50) NOT NULL,
+    [LastName]    VARCHAR (50) NOT NULL,
+    [Email]       VARCHAR (50) NOT NULL,
+    [Password]    VARCHAR (50) NOT NULL,
+    [PhoneNumber] VARCHAR (10) NOT NULL,
+    [Gender]      VARCHAR (10) NULL,
+    [Age]         INT          NOT NULL,
+    [IsloggedIn]  BIT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC),
+    UNIQUE NONCLUSTERED ([PhoneNumber] ASC),
+    UNIQUE NONCLUSTERED ([Email] ASC)
+);
+
