80e4c1de4ef5b394dc3c615375ce95bea660d44c
KD4-sima-83599 <162394288+KD4-sima-83599@users.noreply.github.com>
2025-03-16 15:20:20 +0530

    dbo.Cost,sql uploded

---
+++ dbo.Cost.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Cost] (
+    [CostId]                 INT        IDENTITY (1, 1) NOT NULL,
+    [ParkingCostPerHourBike] FLOAT (53) NOT NULL,
+    [ParkingCostPerHourCar]  FLOAT (53) NOT NULL,
+    PRIMARY KEY CLUSTERED ([CostId] ASC)
+);
+
