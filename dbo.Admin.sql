82a3f17a6793d6730c43cd301f44e58811647824
KD4-sima-83599 <162394288+KD4-sima-83599@users.noreply.github.com>
2025-03-16 15:14:34 +0530

    dbo.Admin.sql uploded

---
+++ dbo.Admin.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Admin] (
+    [AdminId]    INT          IDENTITY (1, 1) NOT NULL,
+    [FirstName]  VARCHAR (50) NOT NULL,
+    [LastName]   VARCHAR (10) NOT NULL,
+    [Email]      VARCHAR (50) NOT NULL,
+    [Password]   VARCHAR (10) NOT NULL,
+    [IsloggedIn] BIT          NOT NULL,
+	UNIQUE (Email),
+    PRIMARY KEY CLUSTERED ([AdminId] ASC)
+);
+
