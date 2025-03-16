96417a297054ce16b7410b611e7d8fccddf56ad8
KD4-sima-83599 <162394288+KD4-sima-83599@users.noreply.github.com>
2025-03-16 15:23:07 +0530

    dbo.Slot.sql uploded

---
+++ dbo.Slot.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Slot] (
+    [SlotId]        INT          IDENTITY (1, 1) NOT NULL,
+    [SlotName]       NVARCHAR(10)          NOT NULL,
+    [IsOccupied]    BIT          NOT NULL,
+    [FloorId]    INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([SlotId] ASC),
+	UNIQUE ([SlotName]),
+    FOREIGN KEY ([FloorId]) REFERENCES [dbo].[Floor] ([FloorId])
+);
+
