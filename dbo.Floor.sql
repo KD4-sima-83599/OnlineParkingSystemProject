9a8f48f637b2ace7cf9493d98d258a80b2f2848d
KD4-sima-83599 <162394288+KD4-sima-83599@users.noreply.github.com>
2025-03-16 15:20:47 +0530

    dbo.Floor.sql uploded

---
+++ dbo.Floor.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Floor] (
+    [Id]            INT IDENTITY (1, 1) NOT NULL,
+    [FloorNumber]   INT NOT NULL,
+    [SlotAvailable] INT NOT NULL,
+    [SlotOccupied]  INT NOT NULL,
+    [TotalSlot]     INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
