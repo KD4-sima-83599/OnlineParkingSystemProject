5b7f5e9ff2bf2845822fe177386edf472e4d4d0f
KD4-sima-83599 <162394288+KD4-sima-83599@users.noreply.github.com>
2025-03-16 15:15:45 +0530

    dbo.Booking.sql uploded

---
+++ dbo.Booking.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Booking] (
+    [BookingId]          INT          IDENTITY (1, 1) NOT NULL,
+    [VehicleNumber]      VARCHAR (10) NOT NULL,
+    [Date]               DATE         NOT NULL,
+    [ParkingCostPerHour] FLOAT (53)   NOT NULL,
+    [VehicleType]        TINYINT      NOT NULL,
+    [StartTime]          DATETIME     NOT NULL,
+    [EndTime]            DATE         NOT NULL,
+    [TotalCost]          FLOAT (53)   NOT NULL,
+    [Slots]              INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([BookingId] ASC)
+);
+
